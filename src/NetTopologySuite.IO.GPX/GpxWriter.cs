89c0e8c5644d37fdf4991a134a87df6ae8b28c92
Loren Halvorson <loren.halvorson@polaris.com>
2019-07-19 06:45:52 -0500

    Custom root namespaces

--- src/NetTopologySuite.IO.GPX/GpxWriter.cs
+++ src/NetTopologySuite.IO.GPX/GpxWriter.cs
@@ -202,6 +202,15 @@ namespace NetTopologySuite.IO
             writer.WriteGpxStartElement("gpx");
             writer.WriteAttributeString("version", "1.1");
             writer.WriteAttributeString("creator", metadata.Creator);
+
+            if (settings.Namespaces.Any())
+            {
+                foreach (var ns in settings.Namespaces)
+                {
+                    writer.WriteAttributeString("xmlns", ns.Prefix, null, ns.NamespaceUri);
+                }
+            }
+
             if (!metadata.IsTrivial)
             {
                 writer.WriteGpxStartElement("metadata");
